46409309cbd8873dde69ec998ea83e62f6c9dbdc
Sharmin <sharmin.idb.info@gmail.com>
2024-01-28 13:12:32 +0600

    js statement

---
+++ JavaScript_Statement/script.js
@@ -0,0 +1,10 @@
+console.log("hello");
+/* let a, b, c;
+        a = 5;
+        b = 6;
+        c = a+b;
+          document.getElementById("demo1").innerHTML = c;*/
+
+// single line comment
+/* multi line
+comment */
