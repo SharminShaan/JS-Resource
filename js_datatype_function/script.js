33c364beab1045b7c5bb91291d4011d2dacd5ea8
Sharmin <sharmin.idb.info@gmail.com>
2024-01-30 17:10:02 +0600

    js datatype & function

---
+++ js_datatype_function/script.js
@@ -0,0 +1,47 @@
+/* datatypes:
+String
+Number
+Boolean
+Array
+Object can contain [Object, array, date]
+BigInt
+undefined
+null
+Symbol
+
+js Object variable can hold any type of data.
+typeof show the datatype 
+*/
+
+/*let x = 5;
+let y = 6;
+let z = 7;
+let myObject = {x: 5, y: 6, z: 7};
+
+function sleep(name,  time) {
+    console.log(name + "is sleeping from " + time);*/
+}
+//sleep();
+//console.log("My name is Jeny");
+
+//console.log(name);
+//sleep("Lily", "10pm");
+//sleep("Mili", "11pm");
+//sleep("Popy", "10:50pm");
+
+
+//return should be the last line of a function.
+
+/*function myFunction(a, b) {
+    return a * b;
+}
+let x = myFunction(4, 3);*/
+
+var carName = "BMW";
+function myFuntion() {
+    var carName = "Volvo";
+    console.log(carName);
+}
+myFuntion();
+
+console.log(carName);
