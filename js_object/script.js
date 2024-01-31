fc0255eca9c18c66b9820b14d79b04d0f7f44f3e
Sharmin <sharmin.idb.info@gmail.com>
2024-01-31 17:08:13 +0600

    js object

---
+++ js_object/script.js
@@ -0,0 +1,28 @@
+/*const car = {
+    name: "Fiat",
+    model: 500,
+    weight: "850kg",
+    color: "White",
+    start: function (){
+        this.drive();
+        console.log("car has started");
+    },
+    drive: function() {
+        console.log("car is driving");
+    }
+};
+console.log(car.weight);
+console.log(car['model']);
+car.start();
+
+let x = "Rahi"; //literal syntax
+let x = new String('Rahi');
+
+let x = 5; //literal syntax
+let x = new number(5);
+console.log(typeof x);
+console.log(x);*/
+
+function displayDate(){
+    document.getElementById("demo").innerHTML = Date();
+}
